0ae1db3aacac579bfedfa69755e9c40e1e47c593
Prince-Mendiratta <prince.mendi@gmail.com>
2021-06-08 16:39:18 +0530

    Add Spec.ts

---
+++ frontend/src/app/third-party-domain/third-party-domain.component.spec.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing'
+import { TranslateModule } from '@ngx-translate/core'
+import { ThirdPartyComponent } from './third-party-domain.component'
+import { of, throwError } from 'rxjs'
+import { ConfigurationService } from '../Services/configuration.service'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+describe('ThirdPartyComponent', () => {
+  let component: ThirdPartyComponent
+  let fixture: ComponentFixture<ThirdPartyComponent>
+  let configurationService: any
+
+  beforeEach(waitForAsync(() => {
+    configurationService = jasmine.createSpyObj('ConfigurationService', ['getApplicationConfiguration'])
+    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { } }))
+    TestBed.configureTestingModule({
+      declarations: [ThirdPartyComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        MatCardModule,
+        MatButtonModule
+      ],
+      providers: [
+        { provide: ConfigurationService, useValue: configurationService }
+      ]
+    })
+      .compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TokenSaleComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should set altcoinName as obtained from configuration', () => {
+    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { altcoinName: 'Coin' } }))
+    component.ngOnInit()
+    expect(component.altcoinName).toBe('Coin')
+  })
+
+  it('should log error on failure in retrieving configuration from backend', fakeAsync(() => {
+    configurationService.getApplicationConfiguration.and.returnValue(throwError('Error'))
+    console.log = jasmine.createSpy('log')
+    component.ngOnInit()
+    expect(console.log).toHaveBeenCalledWith('Error')
+  }))
+})
